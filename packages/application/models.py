5f2e602ac812633228e9fa69b565368f7f844224
agray998 <a.gray.4797@gmail.com>
2023-03-13 13:45:07 +0000

    modifications for running in containers

---
+++ packages/application/models.py
@@ -0,0 +1,28 @@
+from application import db
+
+class Driver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    forename = db.Column(db.String(20))
+    surname = db.Column(db.String(20))
+    packages = db.relationship('Package', backref='driver')
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "forename": self.forename,
+            "surname": self.surname
+        }
+
+class Package(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    address = db.Column(db.String(50), nullable=False)
+    volume = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(10))
+    driver_id = db.Column(db.Integer, db.ForeignKey('driver.id'), nullable=False)
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "address": self.address,
+            "status": self.status
+        }
