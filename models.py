e04b47b94da983997c90a373a23b0396076d7bc1
agray998 <a.gray.4797@gmail.com>
2023-03-13 12:19:01 +0000

    refactor to use single models file

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 from application import db
 
+class Driver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    forename = db.Column(db.String(20))
+    surname = db.Column(db.String(20))
+    packages = db.relationship('Package', backref='driver')
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "forename": self.forename,
+            "surname": self.surname
+        }
+
 class Package(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     address = db.Column(db.String(50), nullable=False)
