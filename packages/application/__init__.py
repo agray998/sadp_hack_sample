86cdedc9e2ada1d31cf7ca5dde2f632ed8b33375
agray998 <a.gray.4797@gmail.com>
2023-03-13 20:48:44 +0000

    fix uri

--- packages/application/__init__.py
+++ packages/application/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from os import getenv
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/qommon"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/qommon"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQL_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
